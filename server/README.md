1c4b85cd9d8155c8bbec5f847fb40df518869911
laytan <Laytanlaats@hotmail.com>
2018-12-01 04:29:05 +0100

    Initial commit, basic prototype

---
+++ server/README.md
@@ -0,0 +1,24 @@
+# Server
+
+## .env
+
+Add the following rules to your .env file:
+
+* SECRET - The secret used for jsonwebtokens
+* ALLOWED_ORIGIN - From which addres can a client connect
+* DATABASE_USER
+* DATABASE_PASSWORD
+* DATABASE_HOST
+* DATABASE_PORT
+
+## Setup
+
+### Start server once
+```
+npm run start
+```
+
+### Automatic restarting of the server
+```
+npm run dev
+```
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'laytan',
-    password: 'laytan',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     insecureAuth : true,
     database: 'listr',
-    port: '3306',
+    port: process.env.DATABASE_PORT,
 });
 
 mysqlConnection.connect(function(err) {
